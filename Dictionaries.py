a182940533b5ea7d03fc176608f698450832858c
LAIBA ASHRAF <112387588+LAIBAASHRAF@users.noreply.github.com>
2022-09-12 11:21:01 -0700

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+dic = {"laiba": "Python", "Nadia":"java", "Noor": "JS","Nida":  {"breakfast": "eggs", "lunch":"salad", "dinner": "kabab"}}
+print(type(dic))
+print(dic)
+print(dic["Nida"])
+dic["mona"]="bongiya marti ha bs"
+print(dic)
+print(dic.items())
+print(dic.keys())
+print(dic.values())
+print(dic.update("Nida"))
+var = {"soha": "Css"}
+# print(dic.update())
+print(var)
